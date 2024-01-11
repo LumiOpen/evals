48320456ceac35e0c04c7aa9fe27f901169fcab9
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2024-01-11 15:15:57 +0900

    Add vocab size and more model-data pairs

--- fertility/run.sh
+++ fertility/run.sh
@@ -3,18 +3,25 @@
 model_langs=(
     "gpt2:eng"
     "bigscience/bloom:fra"
+    "bigscience/bloom:spa"
     "bigscience/bloom:deu"
+    "TurkuNLP/gpt3-finnish-small:fin"
+    "LumiOpen/Poro:fin"
+    "LumiOpen/Poro:eng"
+    "AI-Sweden-Models/gpt-sw3-126m:swe"
+    "AI-Sweden-Models/gpt-sw3-126m:nob"
+    "AI-Sweden-Models/gpt-sw3-126m:dan"
+    "AI-Sweden-Models/gpt-sw3-126m:isl"
+    "PlanTL-GOB-ES/gpt2-base-bne:spa"
 )
 
 for model_lang in "${model_langs[@]}"; do
     model="${model_lang%%:*}"
     lang="${model_lang#*:}"
+    size=$(python3 vocab_size.py $model 2>/dev/null)
     result=$(
 	python3 fertility.py $model flores200_dataset/dev/$lang* 2>/dev/null \
 	    | perl -pe 's/^fertility\s+//; s/ /\t/'
     )
-    echo "$model"$'\t'"$lang"$'\t'"$result"
+    echo "$model"$'\t'"$size"$'\t'"$lang"$'\t'"$result"
 done
-    
-#python3 fertility.py gpt2 flores200_dataset/dev/eng_Latn.dev 2>/dev/null
-
