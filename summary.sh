865074809ea292b2eee0727075e82ad1b9bd55c4
jonabur <135807120+jonabur@users.noreply.github.com>
2024-01-24 16:42:25 +0200

    show evals even if data file is missing

--- summary.sh
+++ summary.sh
@@ -52,47 +52,62 @@ fi
 
 echo
 
+echo -n "             arc_easy: "
 if [ -f $DIR/arc_easy.json ] ; then
-    echo -n "             arc_easy: "
     cat $DIR/arc_easy.json | jq .results.arc_easy.acc | awk '{print $1 * 100}'
+else
+    echo na
 fi
 
+echo -n "                boolq: "
 if [ -f $DIR/boolq.json ] ; then
-    echo -n "                boolq: "
     cat $DIR/boolq.json | jq .results.boolq.acc | awk '{print $1 * 100}'
+else
+    echo na
 fi
 
+echo -n "              nq_open: "
 if [ -f $DIR/nq_open.json ] ; then
-    echo -n "              nq_open: "
     cat $DIR/nq_open.json | jq .results.nq_open.em | awk '{print $1 * 100}'
+else
+    echo na
 fi
 
+echo -n "     openbookqa (acc): "
 if [ -f $DIR/openbookqa.json ] ; then
-    echo -n "     openbookqa (acc): "
     cat $DIR/openbookqa.json | jq .results.openbookqa.acc | awk '{print $1 * 100}'
+else
+    echo na
 fi
 
-
+echo -n "           piqa (acc): "
 if [ -f $DIR/piqa.json ] ; then
-    echo -n "           piqa (acc): "
     cat $DIR/piqa.json | jq .results.piqa.acc | awk '{print $1 * 100}'
+else
+    echo na
 fi
 
+echo -n "          squad2 (f1): "
 if [ -f $DIR/squad2.json ] ; then
-    echo -n "          squad2 (f1): "
     cat $DIR/squad2.json | jq .results.squad2.f1
+else
+    echo na
 fi
 
+echo -n "             triviaqa: "
 if [ -f $DIR/triviaqa.json ] ; then
-    echo -n "             triviaqa: "
     cat $DIR/triviaqa.json | jq .results.triviaqa.em | awk '{print $1 * 100}'
+else
+    echo na
 fi
 
 echo
 
+echo -n "        toxigen (acc): "
 if [ -f $DIR/toxigen.json ] ; then
-    echo -n "        toxigen (acc): "
     cat $DIR/toxigen.json | jq .results.toxigen.acc | awk '{print $1 * 100}'
+else
+    echo na
 fi
 
 
@@ -100,30 +115,40 @@ fi
 
 echo
 
+echo -n "     humaneval_pass@1: "
 if [ -f $DIR/humaneval_pass@1.json ] ; then
-    echo -n "     humaneval_pass@1: "
     cat $DIR/humaneval_pass@1.json | jq '.humaneval."pass@1"' | awk '{print $1 * 100 }'
+else
+    echo na
 fi
 
+echo -n "    humaneval_pass@10: "
 if [ -f $DIR/humaneval_pass@10.json ] ; then
-    echo -n "    humaneval_pass@10: "
     cat $DIR/humaneval_pass@10.json | jq '.humaneval."pass@10"' | awk '{print $1 * 100 }'
+else
+    echo na
 fi
+echo -n "          mbpp_pass@1: "
 if [ -f $DIR/mbpp_pass@1.json ] ; then
-    echo -n "          mbpp_pass@1: "
     cat $DIR/mbpp_pass@1.json | jq '.mbpp."pass@1"' | awk '{print $1 * 100 }'
+else
+    echo na
 fi
 
+echo -n "         mbpp_pass@10: "
 if [ -f $DIR/mbpp_pass@10.json ] ; then
-    echo -n "         mbpp_pass@10: "
     cat $DIR/mbpp_pass@10.json | jq '.mbpp."pass@10"' | awk '{print $1 * 100 }'
+else
+    echo na
 fi
 
 echo 
 
+echo finbench_3shot:
 if [ -f $DIR/finbench_3shot.json ] ; then 
-    echo finbench_3shot:
     cat $DIR/finbench_3shot.json | jq -r '.results | to_entries | .[] | [.key, .value.multiple_choice_grade] | @csv'
+else
+    echo na
 fi
 
 
