996e77ef50b505a08f4964774a307d22bf59b545
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2024-03-20 07:59:48 +0200

    Multiple texts

--- fertility/fertility.py
+++ fertility/fertility.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
+import os
 import regex
 
 from argparse import ArgumentParser
@@ -16,7 +17,7 @@ def argparser():
     ap.add_argument('--no-split', action='store_true',
                     help='process text without splitting lines')
     ap.add_argument('tokenizer')
-    ap.add_argument('text')
+    ap.add_argument('text', nargs='+')
     return ap
 
 
@@ -25,20 +26,29 @@ def main(argv):
 
     tokenizer = AutoTokenizer.from_pretrained(args.tokenizer)
 
-    with open(args.text) as f:
-        text = f.read()
+    total_token_count, total_word_count = 0, 0
+    for fn in args.text:
+        with open(fn) as f:
+            text = f.read()
 
-    if args.no_split:
-        lines = [text]
-    else:
-        lines = text.splitlines()
+        if args.no_split:
+            lines = [text]
+        else:
+            lines = text.splitlines()
 
-    token_count, word_count = 0, 0
-    for line in lines:
-        token_count += len(tokenizer(line).input_ids)
-        word_count += len(WORD_RE.findall(line))
+        token_count, word_count = 0, 0
+        for line in lines:
+            token_count += len(tokenizer(line).input_ids)
+            word_count += len(WORD_RE.findall(line))
 
-    print(f'fertility {token_count}/{word_count} ({token_count/word_count:.2f})')
+        print(f'{os.path.basename(fn)} {token_count}/{word_count} '
+              f'({token_count/word_count:.2f})')
+
+        total_token_count += token_count
+        total_word_count += word_count
+
+    print(f'TOTAL {total_token_count}/{total_word_count} '
+          f'({total_token_count/total_word_count:.2f})')
 
 
 if __name__ == '__main__':
