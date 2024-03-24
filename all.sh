2e39105fb255563e61b1395bb6814ca4511c3ff7
jonabur <135807120+jonabur@users.noreply.github.com>
2024-03-24 11:08:08 +0200

    switch partition again

--- all.sh
+++ all.sh
@@ -1,3 +1,1 @@
-echo $1
-sleep 1
-python main.py --partition small-g --model $1 arc_challenge hellaswag mmlu truthfulqa_mc winogrande gsm8k humaneval_pass@1 humaneval_pass@10 mbpp_pass@1 mbpp_pass@10 arc_easy boolq nq_open openbookqa piqa triviaqa toxigen squad2 finbench_3shot
+python main.py --partition standard-g --model $1 finbench_3shot arc_challenge_fi arc_challenge hellaswag mmlu truthfulqa_mc winogrande gsm8k humaneval_pass@1 humaneval_pass@10 mbpp_pass@1 mbpp_pass@10 arc_easy boolq nq_open openbookqa piqa triviaqa toxigen squad2
