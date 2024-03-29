875ad34db37178b575fb741c112003fc02f45714
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2024-03-29 13:02:19 +0200

    Generate char-ppl table

--- generate_summaries.sh
+++ generate_summaries.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+# Mapping between model names and labels
+declare -A NAME_MAP=(
+    [Poro-34B]='Poro 34B'
+    [gpt3-finnish-13B]='FinGPT 13B'
+    [gpt3-finnish-8B]='FinGPT 8B'
+    [falcon-40b]='Falcon 40B'
+    [llama-30b]='Llama 33B'
+    [mpt-30b]='MPT 30B'
+    [starcoderbase]='StarCoder'
+)
+
+# Order of models in tables
+ORDER_STR='Poro 34B,Llama 33B,MPT 30B,Falcon 40B,FinGPT 8B,FinGPT 13B,StarCoder'
+
+# Format NAME_MAP as "key:value,key:value,..."
+NAME_MAP_STR=""
+for key in "${!NAME_MAP[@]}"; do
+    value=${NAME_MAP[$key]}
+    NAME_MAP_STR+="$key:$value,"
+done
+NAME_MAP_STR=${NAME_MAP_STR%,}    # trailing ","
+
 echo 'This script will rewrite summaries/, latex_tables/, and figures/. Continue? (y/n)'
 while true; do
     read answer
@@ -10,14 +32,10 @@ while true; do
     esac
 done
 
-rm -rf summaries
-mkdir summaries
-
-rm -rf latex_tables
-mkdir latex_tables
-
-rm -rf figures
-mkdir figures
+for d in summaries latex_tables figures; do
+    rm -rf "$d"
+    mkdir "$d"
+done
 
 set -euo pipefail
 
@@ -55,3 +73,33 @@ python3 plot_poro_progression.py \
 	summaries/finbench/poro-34b_step{25632,48672,70128,95328,119232,143712,166752,190656,214560,238418}.tsv \
 	summaries/finbench/TurkuNLP_gpt3-finnish-8B.tsv \
 	summaries/finbench/TurkuNLP_bloom-finnish-176b.tsv
+
+### Perplexity
+
+# Character-level perplexity TSV summaries
+mkdir summaries/char-ppl
+for f in perplexity/results/*.txt; do
+    egrep 'mean cppl:' $f \
+	| perl -pe 's/^fin.*: /Finnish\t/' \
+	| perl -pe 's/^eng.*: /English\t/' \
+	| perl -pe 's/^code.*: /Code\t/' \
+	| perl -pe 's/^overall.*: /Average\t/' \
+	       > summaries/char-ppl/$(basename $f .txt).tsv
+done
+
+# Character-level perplexity table
+o=latex_tables/char-ppl.tex
+
+data=$(ls summaries/char-ppl/*.tsv \
+	   | perl -pe 's/(.*\/(.*)\.tsv)/$2:$1/' | sort -n | tr '\n' ' ')
+
+echo "$data"
+
+python3 make_latex_table.py \
+	--caption 'Character-level perplexity' \
+	--label 'tab:char-ppl' \
+	--decimals 3 \
+	--name-map "$NAME_MAP_STR" \
+	--order "$ORDER_STR" \
+	$data \
+	> "$o"
