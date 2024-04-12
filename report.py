0b588215094da9017fdec9f45baf0a7fbb44d45a
jonabur <135807120+jonabur@users.noreply.github.com>
2024-04-12 14:14:19 +0300

    add wip categorization

--- report.py
+++ report.py
@@ -4,6 +4,56 @@ from evals.evals import evals
 from evals.results import Result
 
 
+sections = [
+    {
+        "name": "english core",
+        "evals": [
+            "arc_challenge.json",
+            "gsm8k.json",
+            "hellaswag.json",
+            "mmlu.json",
+            "truthfulqa_mc.json",
+            "winogrande.json",
+        ],
+    }, {
+        "name": "finnish",
+        "evals": [
+            "finbench_3shot.json",
+            "arc_challenge_fi.json",
+        ],
+    }, {
+        "name": "nordic other",
+        "evals": [
+            "arc_challenge_da.json",
+            "arc_challenge_nb.json",
+            "arc_challenge_sv.json",
+        ],
+    }, {
+        "name": "code",
+        "evals": [
+            "humaneval_pass@10.json",
+            "humaneval_pass@1.json",
+            "mbpp_pass@10.json",
+            "mbpp_pass@1.json",
+        ],
+    }, {
+        "name": "english extended",
+        "evals": [
+            "arc_easy.json",
+            "boolq.json",
+            "nq_open.json",
+            "openbookqa.json",
+            "piqa.json",
+            "squad2.json",
+            "triviaqa.json",
+        ],
+    }, {
+        "name": "bias",
+        "evals": [
+            "toxigen.json",
+        ],
+    }
+]
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
