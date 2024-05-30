a32577bd60e5dde44448ce91eee1a154988d80d6
jonabur <135807120+jonabur@users.noreply.github.com>
2024-05-30 14:18:50 +0300

    update

--- all.sh
+++ all.sh
@@ -1,1 +1,2 @@
-python main.py --partition standard-g --model $1 finbench_3shot arc_challenge_fi arc_challenge hellaswag mmlu truthfulqa_mc winogrande gsm8k humaneval_pass@1 humaneval_pass@10 mbpp_pass@1 mbpp_pass@10 arc_easy boolq nq_open openbookqa piqa triviaqa toxigen squad2
+python main.py --time 04:00:00 --partition standard-g --model $1 finbench_3shot arc_challenge_fi arc_challenge_da arc_challenge_nb arc_challenge_sv arc_challenge truthfulqa_mc winogrande  humaneval_pass@1 mbpp_pass@1 arc_easy boolq nq_open openbookqa piqa triviaqa toxigen 
+python main.py --time 24:00:00 --partition standard-g --model $1 mmlu humaneval_pass@10 mbpp_pass@10 squad2 hellaswag gsm8k
