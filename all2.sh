b6e089ddfc9afb6fca4ab41c4a6fdc3323dd988c
jonabur <135807120+jonabur@users.noreply.github.com>
2024-05-30 14:20:01 +0300

    add europa and v2 specific scripts

---
+++ all2.sh
@@ -0,0 +1,2 @@
+python main.py --time 04:00:00 --partition standard-g --model $1 arc_challenge2 winogrande2 truthfulqa_mc2
+python main.py --time 24:00:00 --partition standard-g --model $1 mmlu2 hellaswag2 gsm8k2
---
+++ europa.sh
@@ -0,0 +1,1 @@
+python main.py --time 04:00:00 --partition standard-g --model $1 arc_challenge_de arc_challenge_el arc_challenge_es arc_challenge_hu arc_challenge_it arc_challenge_pl arc_challenge_pt
