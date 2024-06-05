2a9aec23a5cdd8f2b7a95fd7ed6acf70d4ddf318
jonabur <135807120+jonabur@users.noreply.github.com>
2024-06-05 09:42:54 +0300

    update default set of tests and timings for 70b

--- all.sh
+++ all.sh
@@ -1,2 +1,10 @@
-python main.py --time 04:00:00 --partition standard-g --model $1 finbench_3shot arc_challenge_fi arc_challenge_da arc_challenge_nb arc_challenge_sv arc_challenge truthfulqa_mc winogrande  humaneval_pass@1 mbpp_pass@1 arc_easy boolq nq_open openbookqa piqa triviaqa toxigen 
-python main.py --time 24:00:00 --partition standard-g --model $1 mmlu humaneval_pass@10 mbpp_pass@10 squad2 hellaswag gsm8k
+# we decine shorter time periods to allow evals to get scheduled more quickly
+# via backfill when the cluster is full.
+# times are based on a 70B model
+# don't try to cut it too close, allow 1-2 hours additional time.
+python main.py --time 04:00:00 --partition standard-g --model $1 arc_challenge_mt_fi arc_challenge_mt_da arc_challenge_mt_nb arc_challenge_mt_sv arc_challenge truthfulqa_mc winogrande  humaneval_pass@1 mbpp_pass@1 arc_easy boolq nq_open openbookqa piqa toxigen 
+python main.py --time 04:00:00 --partition standard-g --model $1 arc_challenge_mt_de arc_challenge_mt_el arc_challenge_mt_es arc_challenge_mt_hu arc_challenge_mt_it arc_challenge_mt_pl arc_challenge_mt_pt
+python main.py --time 06:00:00 --partition standard-g --model $1 finbench_3shot
+python main.py --time 10:00:00 --partition standard-g --model $1 mmlu
+python main.py --time 24:00:00 --partition standard-g --model $1 humaneval_pass@10 squad2 hellaswag gsm8k triviaqa
+python main.py --time 48:00:00 --partition standard-g --model $1 mbpp_pass@10
