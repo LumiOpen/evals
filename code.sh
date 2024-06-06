c4f77510141ec0dedf6332bd83d976ae4f61e61c
jonabur <135807120+jonabur@users.noreply.github.com>
2024-06-06 16:25:49 +0300

    add code helper script

---
+++ code.sh
@@ -0,0 +1,8 @@
+QUEUE=standard-g
+# we decine shorter time periods to allow evals to get scheduled more quickly
+# via backfill when the cluster is full.
+# times are based on a 70B model
+# don't try to cut it too close, allow 1-2 hours additional time.
+python main.py --time 04:00:00 --partition $QUEUE --model $1 humaneval_pass@1 mbpp_pass@1
+python main.py --time 24:00:00 --partition $QUEUE --model $1 humaneval_pass@10
+python main.py --time 48:00:00 --partition $QUEUE --model $1 mbpp_pass@10
