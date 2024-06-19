d41057c108179c0665b690c0f869e9cefadb0efb
jonabur <135807120+jonabur@users.noreply.github.com>
2024-06-19 09:34:50 +0300

    add icelandic eval

--- all.sh
+++ all.sh
@@ -3,7 +3,7 @@ QUEUE=standard-g
 # via backfill when the cluster is full.
 # times are based on a 70B model
 # don't try to cut it too close, allow 1-2 hours additional time.
-python main.py --time 04:00:00 --partition $QUEUE --model $1 arc_challenge_mt_fi arc_challenge_mt_da arc_challenge_mt_nb arc_challenge_mt_sv arc_challenge truthfulqa_mc winogrande arc_easy boolq nq_open openbookqa piqa toxigen 
+python main.py --time 04:00:00 --partition $QUEUE --model $1 arc_challenge_mt_fi arc_challenge_mt_da arc_challenge_mt_is arc_challenge_mt_nb arc_challenge_mt_sv arc_challenge truthfulqa_mc winogrande arc_easy boolq nq_open openbookqa piqa toxigen 
 python main.py --time 04:00:00 --partition $QUEUE --model $1 arc_challenge_mt_de arc_challenge_mt_el arc_challenge_mt_es arc_challenge_mt_hu arc_challenge_mt_it arc_challenge_mt_pl arc_challenge_mt_pt
 python main.py --time 06:00:00 --partition $QUEUE --model $1 finbench_3shot
 python main.py --time 10:00:00 --partition $QUEUE --model $1 mmlu
