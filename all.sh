a5c409dcd0b16377b48c7c7813a1ed775e61ef9b
Akseli Reunamo <akseli.y.reunamo@utu.fi>
2024-11-11 13:24:51 +0200

    remove squad2

--- all.sh
+++ all.sh
@@ -7,4 +7,8 @@ python main.py --time 04:00:00 --partition $QUEUE --model $1 arc_challenge_mt_fi
 python main.py --time 04:00:00 --partition $QUEUE --model $1 arc_challenge_mt_de arc_challenge_mt_el arc_challenge_mt_es arc_challenge_mt_hu arc_challenge_mt_it arc_challenge_mt_pl arc_challenge_mt_pt arc_challenge_mt_fr arc_challenge_mt_nl
 python main.py --time 06:00:00 --partition $QUEUE --model $1 finbench_3shot
 python main.py --time 10:00:00 --partition $QUEUE --model $1 mmlu
-python main.py --time 24:00:00 --partition $QUEUE --model $1 squad2 hellaswag gsm8k triviaqa
+#skipping squad as its not working
+#python main.py --time 24:00:00 --partition $QUEUE --model $1 squad2 hellaswag gsm8k triviaqa
+python main.py --time 07:00:00 --partition $QUEUE --model $1 hellaswag gsm8k triviaqa
+
+
