ef3677eae04b174945b0f493fd5930fa8b37c384
jonabur <135807120+jonabur@users.noreply.github.com>
2025-02-20 08:57:29 +0200

    add history reporting mode

--- watch.py
+++ watch.py
@@ -1,4 +1,6 @@
 import argparse
+import collections
+from datetime import datetime, timedelta
 import json
 import os
 import subprocess
@@ -45,8 +47,6 @@ def read_command_log():
     return entries
 
 def get_running_jobs():
-    
-    #username = os.getlogin()
     command = ['squeue', '--me', '-o', '%i %T %j']
     process = subprocess.Popen(command, stdout=subprocess.PIPE)
     output, error = process.communicate()
@@ -67,7 +67,76 @@ def get_running_jobs():
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--once', action='store_true', help='run once and exit')
+    parser.add_argument('--hist', action='store_true', help='list recently completed jobs')
+    parser.add_argument('--days', default=3, type=int, help='days of history')
+
+
     args = parser.parse_args()
+
+    if args.hist:
+        entries = read_command_log()
+
+        # workaround for bad entry in my log
+        if None in entries:
+            del entries[None]
+
+        # find all jobs in the last N days
+        report = collections.defaultdict(list)
+        now = datetime.now()
+        delta = timedelta(days=args.days)
+        for k in reversed(sorted(entries.keys())):
+            entry = entries[k]
+            timestamp = datetime.strptime(entry["timestamp"], "%Y-%m-%d %H:%M:%S")
+            if now - timestamp <= delta:
+                report[entry["model"]].append(entry)
+
+        running_jobs = set(get_running_jobs())
+        for model in sorted(report.keys()):
+            entries = sorted(report[model], key=lambda x: x["eval"])
+
+            print(f"Model: {model}")
+            print(f"Results dir: {os.path.dirname(entries[0]['output_file'])}")
+
+            completed = []
+            incomplete = []
+            running = []
+            for entry in entries:
+                if entry["job_id"] in running_jobs:
+                    running.append(entry)
+                elif os.path.exists(entry["output_file"]):
+                    completed.append(entry)
+                else:
+                    incomplete.append(entry)
+            if completed:
+                print(f"Completed:")
+                for entry in completed:
+                    print(f"    {entry['eval']}")
+            if running:
+                print("Running/Queued:")
+                for entry in running:
+                    print(f"    {entry['eval']} {entry['job_id']}")
+
+            # first let's filter these down to evals that have not
+            # subsequently succeeded.
+            complete_failures = []
+            if incomplete:
+                succeeded = set([i["eval"] for i in completed])
+                complete_failures = [i for i in incomplete if i["eval"] not in succeeded]
+
+            if complete_failures:
+                # these jobs are not in queue and no results exist
+                print("Failed:")
+                for entry in complete_failures:
+                    if os.path.exists(entry['err_log']):
+                        print(f"    {entry['eval']} {entry['err_log']}")
+                    else:
+                        print(f"    {entry['eval']} no err log, cancelled?")
+            print("")
+            
+                
+            
+            
+        return
 
 
     jobs = []
