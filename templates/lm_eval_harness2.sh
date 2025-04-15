7ac53ddda3de7488259b3f58171efa1fe8e203f8
ezosa <elaine.zosa@gmail.com>
2025-04-15 16:49:42 +0300

    added langdetect installation for ifeval_fi

--- templates/lm_eval_harness2.sh
+++ templates/lm_eval_harness2.sh
@@ -148,6 +148,7 @@ pip install jinja2 --upgrade  # missing requirement?
 pip install sentencepiece --upgrade  # required for 01-ai/Yi
 pip install protobuf --upgrade       # required for Llama 2 tokenizer
 pip install tiktoken --upgrade
+pip install langdetect --upgrade # required for ifeval_fi and ifeval_sv
 
 pip install -e .
 pip install -e .[math]
