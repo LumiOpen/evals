ae989da9b8f94e221d38440d23dfa253cd40e498
jonabur <135807120+jonabur@users.noreply.github.com>
2025-08-22 14:07:40 +0300

    add unstripped humaneval runs

--- code.sh
+++ code.sh
@@ -3,6 +3,6 @@ QUEUE=standard-g
 # via backfill when the cluster is full.
 # times are based on a 70B model
 # don't try to cut it too close, allow 1-2 hours additional time.
-python main.py --time 12:00:00 --partition $QUEUE --model $1 humaneval_pass@1 mbpp_pass@1
-python main.py --time 24:00:00 --partition $QUEUE --model $1 humaneval_pass@10
+python main.py --time 12:00:00 --partition $QUEUE --model $1 humaneval_pass@1 mbpp_pass@1 humaneval-unstripped_pass@1
+python main.py --time 24:00:00 --partition $QUEUE --model $1 humaneval_pass@10 humaneval-unstripped_pass@10
 python main.py --time 48:00:00 --partition $QUEUE --model $1 mbpp_pass@10
