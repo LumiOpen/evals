6037a6cf233758fe78eff97bdabbbcbebac7cc15
Daniel Zautner <daniel.zautner@silo.ai>
2025-11-05 15:47:18 +0200

    Fix last stop_newline variable reference

--- helmet/model_utils.py
+++ helmet/model_utils.py
@@ -918,7 +918,7 @@ class HFModel(LLM):
         # use the default if possible, append if necessary
         stop_token_ids = self.model.generation_config.eos_token_id
         stop_token_ids = [stop_token_ids] if not isinstance(stop_token_ids, list) else stop_token_ids
-        if stop_newline:
+        if stop_new_line:
             stop = list(set(["\n", "Ċ", "ĊĊ", "<0x0A>"]))
             stop_token_ids = list(set([self.tokenizer.convert_tokens_to_ids(stop_token) for stop_token in stop] + stop_token_ids))
             if "llama" in model_name.lower():
