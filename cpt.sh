b728ea8c3ff692250e34dc5977186102f12d1eb7
Daniel Zautner <daniel.zautner@silo.ai>
2026-02-16 15:06:52 +0200

    migrate to lumi ai factory container

--- cpt.sh
+++ cpt.sh
@@ -2,7 +2,7 @@ QUEUE=standard-g
 PROJECT=project_462000963
 
 # this can go very long in models that are not good at this language, i guess?
-python main.py --time 12:00:00 --project $PROJECT --partition $QUEUE --model $1 flores200_trans_en_fi flores200_trans_fi_en
-python main.py --time 10:00:00 --project $PROJECT --partition $QUEUE --model $1 arc_challenge arc_challenge_mt_fi truthfulqa_mc truthfulqa_mc_mt_fi goldenswag_mt_fi
-python main.py --time 10:00:00 --project $PROJECT --partition $QUEUE --model $1 mmlu mmlu_mt_fi
-python main.py --time 48:00:00 --project $PROJECT --partition $QUEUE --model $1 gsm8k gsm8k_mt_fi hellaswag hellaswag_mt_fi goldenswag
+python3 main.py --time 12:00:00 --project $PROJECT --partition $QUEUE --model $1 flores200_trans_en_fi flores200_trans_fi_en
+python3 main.py --time 10:00:00 --project $PROJECT --partition $QUEUE --model $1 arc_challenge arc_challenge_mt_fi truthfulqa_mc truthfulqa_mc_mt_fi goldenswag_mt_fi
+python3 main.py --time 10:00:00 --project $PROJECT --partition $QUEUE --model $1 mmlu mmlu_mt_fi
+python3 main.py --time 48:00:00 --project $PROJECT --partition $QUEUE --model $1 gsm8k gsm8k_mt_fi hellaswag hellaswag_mt_fi goldenswag
